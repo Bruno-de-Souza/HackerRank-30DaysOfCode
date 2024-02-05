85b25e9d34cde6eb2969126d11e534f5a0683ad6
Bruno Moreira <brunomoreiramg@gmail.com>
2024-02-04 23:39:02 -0300

    Function solve - Day 2

--- Day 0/helloWorld.js
+++ Day 0/helloWorld.js
@@ -1,6 +1,7 @@
-/*Task
-To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
-You've got this!
+/*
+    Task
+    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and   finally print the value of your variable on a second line.
+    You've got this!
 */
 function processData(inputString) {
     // This line of code prints the first line of output
